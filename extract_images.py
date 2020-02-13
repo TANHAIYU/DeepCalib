1b3c7424400f7dd1dc44f5b9df7fb15294dfbdfe
alexvbogdan <alex.v.bogdan@gmail.com>
2020-02-13 14:44:11 +0900

    Update extract_images.py

--- extract_images.py
+++ extract_images.py
@@ -11,18 +11,17 @@ filename = ""
  
 
 def video_to_frames(video_filename):
-    """Extract frames from video"""
     source_video = cv2.VideoCapture(video_filename)
-    n_total_frames_source = source_video.get(cv2.CAP_PROP_FRAME_COUNT)
-    f = 0
-    while f <= n_total_frames_source:
+    n_frames = source_video.get(cv2.CAP_PROP_FRAME_COUNT)
+    i = 0
+    while i <= n_frames:
         ret, frame = source_video.read()
         if ret:
             cv2.imwrite(SAVE_PATH + "frame_" + str(f) + ".jpg", frame)
         else:
-            f+=1
+            i += 1
             continue
 
-        f += 1
+        i += 1
 
 video_to_frames(filename)
